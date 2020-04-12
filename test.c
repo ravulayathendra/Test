2c71fcd56af282495f40c7d0651702c7d5c37c25
ravulayathendra <ravulayathendra@gmail.com>
2020-04-11 18:21:08 -0700

    New file Created

---
+++ test.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/11/2020 06:15:48 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yathendra (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <studio.in>
+
+int
+main(int argc.char **argv){
+    
+    return 0;
+}
